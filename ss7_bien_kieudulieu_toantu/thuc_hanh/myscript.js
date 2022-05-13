979d5768b3fda57112fdd941540f223c774abcd2
letrong29 <letrong2929Gmail.com>
2022-05-13 08:16:00 +0700

    up_load_ngay-13_5

---
+++ ss7_bien_kieudulieu_toantu/thuc_hanh/myscript.js
@@ -0,0 +1,10 @@
+inputWidth = prompt("Enter_the_width")
+inputHeight = prompt("Enter_the_height")
+
+var Width = parseInt(inputWidth);
+var Height = parseInt(inputHeight);
+var Area = Width * Height
+
+document.write('Area =' + Area)
+
+
